187346a58f659c79d45cc03d0ce4efcde2388ae7
Pascal AUREGAN <pauregan@serial.ch>
2017-08-16 23:33:19 +0200

    removed unused main content

--- src/main/java/com/mycompany/bynder/App.java
+++ src/main/java/com/mycompany/bynder/App.java
@@ -1,8 +1,5 @@
 package com.mycompany.bynder;
 
-import com.bynder.sdk.service.BynderService;
-import com.mycompany.bynder.service.bynder.BynderServiceFactory;
-
 import java.net.MalformedURLException;
 
 /**
@@ -10,14 +7,6 @@ import java.net.MalformedURLException;
  */
 public class App {
     public static void main(String[] args) throws IllegalAccessException, MalformedURLException {
-        BynderService bynderService = BynderServiceFactory.create(
-                        "https://example.bynder.com",
-                        "consumer key",
-                        "consumer secret",
-                        "token",
-                        "token secret");
-
-        // List<BynderMedia> medias = create(bynderService).synchronousQuery();
 
 
     }
