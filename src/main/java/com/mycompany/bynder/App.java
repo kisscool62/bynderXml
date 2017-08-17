bd190ee7e6073446d19c0afc2d591acfebee303e
Pascal AUREGAN <pauregan@serial.ch>
2017-08-17 08:06:49 +0200

    removed hello world dummy comment

--- src/main/java/com/mycompany/bynder/App.java
+++ src/main/java/com/mycompany/bynder/App.java
@@ -2,9 +2,6 @@ package com.mycompany.bynder;
 
 import java.net.MalformedURLException;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) throws IllegalAccessException, MalformedURLException {
 
